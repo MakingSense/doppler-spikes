051fc8947c44943d88c72ff6d1d9e4ecaf98839d
andresmoschini <andresmoschini@gmail.com>
2018-11-22 10:39:02 -0300

    Add more benchmarks for hashset and dictionary with different initializations

--- DictionayVsHashset/DictionaryVsHashset.cs
+++ DictionayVsHashset/DictionaryVsHashset.cs
@@ -48,4 +48,74 @@ public class DictionaryVsHashset
         var generated = new HashSet<string>(Source.Select(x => x.Key), _equalityComparer);
         count = generated.Count;
     }
+
+    [Benchmark]
+    public static void Hashset_with_constructor_without_select()
+    {
+        var generated = new HashSet<string>(SourceKeys, _equalityComparer);
+        count = generated.Count;
+    }
+
+    [Benchmark]
+    public static void Hashset_with_Add()
+    {
+        var generated = new HashSet<string>(_equalityComparer);
+        foreach (var item in SourceKeys)
+        {
+            generated.Add(item);
+        }
+        count = generated.Count;
+    }
+
+    [Benchmark]
+    public static void Hashset_with_UnionWith()
+    {
+        var generated = new HashSet<string>(_equalityComparer);
+        generated.UnionWith(SourceKeys);
+        count = generated.Count;
+    }
+
+    [Benchmark]
+    public static void Dictionary_with_Add()
+    {
+        var generated = new Dictionary<string, bool>(_equalityComparer);
+        foreach (var item in Source)
+        {
+            generated.Add(item.Key, item.Value);
+        }
+        count = generated.Count;
+    }
+
+
+    [Benchmark]
+    public static void Dictionary_with_Add_with_predefined_capacity()
+    {
+        var generated = new Dictionary<string, bool>(_items, _equalityComparer);
+        foreach (var item in Source)
+        {
+            generated.Add(item.Key, item.Value);
+        }
+        count = generated.Count;
+    }
+
+    [Benchmark]
+    public static void Dictionary_with_Indexer()
+    {
+        var generated = new Dictionary<string, bool>(_equalityComparer);
+        foreach (var item in Source)
+        {
+            generated[item.Key] = item.Value;
+        }
+        count = generated.Count;
+    }
+
+    [Benchmark]
+    public static void Dictionary_with_ToDictionary()
+    {
+        var generated = Source.ToDictionary(
+            x => x.Key,
+            x => x.Value,
+            _equalityComparer);
+        count = generated.Count;
+    }
 }
